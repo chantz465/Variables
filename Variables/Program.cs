9dab0efbdbd6f60da23a5fcd05262c7c56d08a5a
Chance Gardner <Chantz465@gmail.com>
2022-04-21 14:47:52 -0500

    first add

---
+++ Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fristName = "";
+            int age = 0;
+            bool isMale = false;
+            char likesBacon = 'Y';
+            double howManyMilesOnCar = 0;
+            decimal dogeWallet = 0.00m;
+
+
+            age = 25;
+            fristName = "Chance";
+            isMale = true;
+            howManyMilesOnCar = 125987.9;
+            dogeWallet = 5884747.324978m;
+
+            Console.WriteLine("The man is " + age + ", his name is " + fristName + ", does he like bacon " + likesBacon + ", is he a he " + isMale + ", his car has " + howManyMilesOnCar + " on it, and last but not least his doge wallet has " + dogeWallet);
+
+
+        }
+    }
+}
